9b339c7276043d1f8c1fde4bce8eb2d8a2d8dfae
yihui-he <yihuihe@foxmail.com>
2016-09-08 16:33:09 +0800

    sed

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ sudo cp lib64/libcudnn.so.4.0.7 $cudapath/lib64/libcudnn.so.4.0.7
 sudo cp include/cudnn.h $cudapath/include
 cd $cudapath/lib64
 sudo ln -s libcudnn.so.4.0.7 libcudnn.so.4
+sudo rm libcudnn.so
 sudo ln -s libcudnn.so.4 libcudnn.so
+sudo sed -i '115s/^#/\/\//' /usr/local/cuda/include/host_config.h
 ldconfig
